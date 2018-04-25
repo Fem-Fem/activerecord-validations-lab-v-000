2ae2e363dc0405fbffcc6e1f0b6c64099029738e
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-25 23:07:07 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :title, presence: true if: not_clickbait
+  validates :title, presence: true if :not_clickbait
 
   def not_clickbait
     if Post.title.include? "Won't Believe"
