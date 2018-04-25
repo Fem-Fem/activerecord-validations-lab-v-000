fb7d253f6f5a58d095412bd47b92602da108606c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-25 23:12:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,11 +14,13 @@ class MyValidator < ActiveModel::Validator
 end
 
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :title, presence: true if :not_clickbait
+  # validates :title, presence: true if :not_clickbait
 
   # def not_clickbait
   #   return self.title
